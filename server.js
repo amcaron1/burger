9ba51d5cb86346d040c50a4b9ff3aeb0efc27bd9
Andre <amcaron1@msn.com>
2018-11-02 18:09:58 -0500

    pay

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/payroll.js");
+
 
 app.use(routes);
 
